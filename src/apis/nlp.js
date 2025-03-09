9b001462e5e87747900213786388af6c8b3635d9
Rushikesh Barkale <rushikesh@turnkey.work>
2025-03-09 16:35:14 +0530

    updated BE nlp url

--- src/apis/nlp.js
+++ src/apis/nlp.js
@@ -7,10 +7,12 @@ import { API_URL } from "../config/config";
 //for docker use below
 // const API_BASE_URL = "http://localhost:8081/api/nlp";
 
+//for prod use
+const API_BASE_URL = `${API_URL}nlp`;
+
 export const processQuery = async (query, page = 1, itemsPerPage = 12) => {
   try {
-    // const response = await axios.post(`${API_BASE_URL}/processquery`, {
-    const response = await axios.post(`${API_URL}/processquery`, {
+    const response = await axios.post(`${API_BASE_URL}/processquery`, {
       query,
       page,
       items_per_page: itemsPerPage,
